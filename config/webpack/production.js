2b6892e41715413cfea4d41a07b41b128d1c4b1d
Козельский Павел Сергеевич <pkozelskiy@n-core.ru>
2017-04-11 21:36:47 +0300

    [widget-2] fix css

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,15 @@
 import webpackMerge from 'webpack-merge';
+import CompressionPlugin from 'compression-webpack-plugin';
 import getBaseConfig from './base';
 
 export default () => webpackMerge(getBaseConfig(), {
+  plugins: [
+      new CompressionPlugin({
+        asset: "[path].gz[query]",
+        algorithm: "gzip",
+        test: /\.(js|html)$/,
+        threshold: 10240,
+        minRatio: 0.8
+      })
+  ],
 });
